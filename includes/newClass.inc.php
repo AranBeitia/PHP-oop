9ade5a813839d1d1b54d61a8f3447d9f41424c70
AranBeitia <arantza.beitia@gmail.com>
2021-11-23 19:51:37 +0100

    start

---
+++ includes/newClass.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+class Person {
+	// Properties and methods goes here
+	private $first = "Daniel";
+	private $last = "Nielsen";
+	private $age = "28";
+}
+
+class Pet {
+	public function owner (){
+		$a = "Hi there!";
+		return $a;
+	}
+}
