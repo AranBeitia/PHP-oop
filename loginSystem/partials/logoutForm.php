a2d71fb81319c0fa26b83fee4f64848541fd0b49
AranBeitia <arantza.beitia@gmail.com>
2021-11-25 20:08:19 +0100

    login form

---
+++ loginSystem/partials/logoutForm.php
@@ -0,0 +1,14 @@
+<form action="includes/signup.inc.php" method="post">
+	<h1>Create Account</h1>
+	<div class="social-container">
+		<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+		<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+		<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+	</div>
+	<span>or use your email for registration</span>
+	<input type="text" name="uid" placeholder="Username" />
+	<input type="password" name="psw" placeholder="Password" />
+	<input type="password" name="pswrepeat" placeholder="Repeat Password" />
+	<input type="email" name="email" placeholder="Email" />
+	<button type="submit" name="submit">Sign Up</button>
+</form>
